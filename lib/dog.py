00fde960944f91186a2dca0caceccdf7ef7ade3d
Eugene Tae Joo Jeong <ejeong24@gmail.com>
2023-06-05 22:57:13 -0400

    passed testing

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,30 @@
 from models import Dog
 
-def create_table(base):
-    pass
+## Tips and Tricks
+# The bodies of all functions in dog.py except create_table() and save() should be composed of a single line of code.
+# Read through the pytest error messages to make sure the input and output for your functions match the tests.
+# Remember which attributes are required when designing a SQLAlchemy data model: a __tablename__, a primary_key, and one or more Columns.
+
+def create_table(base, engine):
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter(Dog.name == name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter(Dog.name == name and Dog.breed == breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.add(dog)
+    session.commit()
